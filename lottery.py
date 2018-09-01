cd8d782559bd0225057461ee861ce30c45473214
Albert Lai <albertlai@gmail.com>
2018-08-31 18:17:15 -0700

    use the real NBA odds

--- lottery.py
+++ lottery.py
@@ -17,14 +17,23 @@ LOTTO_BALLS = {
     3: 119,
     4: 88,
     5: 63,
-    6: 45,
-    7: 30,
-    8: 20,
-    9: 13,
-    10: 10,
+    6: 43,
+    7: 28,
+    8: 17,
+    9: 11,
+    10: 8,
     11: 7,
+    12: 6,
+    13: 5,
+    14: 4,
+    15: 3,
 }
 
+# Remove unnecessary lotto slots (we support up to 16)
+for r in range(len(team_names),16):
+    del LOTTO_BALLS[r]
+
+# Run the drawing, removing selected teams
 order = []
 while LOTTO_BALLS:
     N = float(np.sum(LOTTO_BALLS.values()))
@@ -32,6 +41,7 @@ while LOTTO_BALLS:
     order.append(team_names[draw])
     del LOTTO_BALLS[draw]
 
+# Print some gibberish to make it look like the program is thinking really hard
 def print_gibberish(time_to_run, str_len=50):
     start = time()
     while True:
@@ -42,10 +52,11 @@ def print_gibberish(time_to_run, str_len=50):
         else:
             sys.stdout.write(''.join(random.choice(string.uppercase) for x in range(str_len)))
 
+# Output the final draft results
 gibberish_time = 1
 for i, result in enumerate(reversed(order)):
     print_gibberish(gibberish_time)
     sys.stdout.write('\r')
     sys.stdout.flush()
     print('#%d:\t%s' % (len(order)-i, result))
-    gibberish_time += 1
+    gibberish_time += 0.1
